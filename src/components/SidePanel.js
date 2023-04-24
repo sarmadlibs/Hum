1d999c32575eb0096c7e3da31dbfb6cb64568e80
Sarmad <afzalsarmad77@gmail.com>
2023-04-24 23:16:28 +0300

    added file upload functionality to s3 bucket, fixed toast message

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -1,35 +1,77 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "../styles/SidePanel.css";
 import Contact from "./Contact";
+import ProfilePopup from "./ProfilePopup";
+import { FaSearch } from "react-icons/fa";
+import { ToastContainer } from "react-toastify";
+import userImg from "../assets/img/user.png";
 
 function SidePanel({ onSelectChat }) {
+  const [showProfilePopup, setShowProfilePopup] = useState(false);
   const dummyContacts = [
     {
       id: 1,
       name: "John Doe",
-      profilePicture: "https://via.placeholder.com/40",
+      profilePicture: userImg,
     },
     {
       id: 2,
       name: "Jane Smith",
-      profilePicture: "https://via.placeholder.com/40",
+      profilePicture: userImg,
     },
     {
       id: 3,
       name: "Michael Jackson",
-      profilePicture: "https://via.placeholder.com/40",
+      profilePicture: userImg,
     },
     {
       id: 4,
       name: "Harry Potter",
-      profilePicture: "https://via.placeholder.com/40",
+      profilePicture: userImg,
     },
   ];
+
+  const profileImageRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        profileImageRef.current &&
+        !profileImageRef.current.contains(event.target)
+      ) {
+        setShowProfilePopup(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [profileImageRef]);
+
+  const toggleProfilePopup = () => {
+    setShowProfilePopup(!showProfilePopup);
+  };
 
   return (
     <div className="side-panel">
       <div className="side-panel-header">
-        <h3>Chats</h3>
+        <img
+          ref={profileImageRef}
+          src={userImg}
+          alt="User profile"
+          className="user-profile-image"
+          onClick={toggleProfilePopup}
+        />
+        <div className="chat-search">
+          <FaSearch />
+          <input type="text" placeholder="Search" className="search-input" />
+        </div>
+        <ProfilePopup
+          className={showProfilePopup ? "show" : ""}
+          onClose={toggleProfilePopup}
+        />
       </div>
       <div className="side-panel-content">
         {dummyContacts.map((contact) => (
@@ -41,6 +83,7 @@ function SidePanel({ onSelectChat }) {
           />
         ))}
       </div>
+      <ToastContainer />
     </div>
   );
 }
